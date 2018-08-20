45c41d7371850803e088902b2263619fb8507494
Tale <giangnguyen.tale@gmail.com>
2018-08-20 09:01:48 +0700

    added new blog content created by vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: '{life.simplify()}',
+  description: 'less stuff more happiness.',
+  themeConfig: {
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Posts',
+        link: '/posts/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/talenguyen'
+      },
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+footer: Copyright © 2018-present Giang Nguyen
+---
+
+> less stuff, more happiness."
+
+**Graham Hill**
+
+> if you can't explain it simply, you don't understand it well enough.
+
+**Albert Einstein**
